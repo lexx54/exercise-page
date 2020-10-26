df9fceccfed9f1460ebc0af4d75606e714277602
lexx54 <chapelexx54@gmail.com>
2020-10-25 20:04:52 -0400

    finished the clock section

---
+++ script.js
@@ -0,0 +1,5 @@
+import clockFunc from "./clockScript.js"
+
+const $btnContainer=document.querySelector(".btn-container"),
+  $clockContainer=document.querySelector(".clockshow");
+clockFunc($btnContainer,$clockContainer);
