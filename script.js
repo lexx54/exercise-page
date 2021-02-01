7b8d93c4c8dcda70cbbbb798435cedb1b5bacd41
lexx54 <chapelexx54@gmail.com>
2021-02-01 10:39:43 -0400

    Finished webcam detection

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ import responsiveMedia from "./responsive.js";
 import responsiveTester from "./responsivetester.js";
 import userDeviceInfo from "./deviceDetection.js";
 import networkStatus from "./redDetection.js";
+import webCam from "./webcamDetection.js";
 
 
 
@@ -24,6 +25,7 @@ d.addEventListener("DOMContentLoaded",e=>{
   responsiveMedia('gmaps',"(min-width:1024px)",`<a href=https://goo.gl/maps/vc1Wq1WTqQ1nCmyv5>Paris</a>`,`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d167997.34988503883!2d2.2069747513047933!3d48.85899999269729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671d877937b0f%3A0xb975fcfa192f84d4!2sMuseo%20del%20Louvre!5e0!3m2!1ses!2sve!4v1605394910371!5m2!1ses!2sve" width="350" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`)
   responsiveTester("responsive-tester")
   userDeviceInfo('user-device');
+  webCam("webcam");
 })
 d.addEventListener("click",e=>{
   
